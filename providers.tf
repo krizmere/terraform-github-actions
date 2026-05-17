11115a19f4ded2c3a98de2b940df44211be13293
brian <brian@365mechanix.com>
2026-05-17 16:11:13 +1000

    Update GitHub Actions workflow to include environment input and modify Terraform state resource group

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
+    resource_group_name  = "rg-developer-dev"
     storage_account_name = "stcollsysnpeaue00"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
