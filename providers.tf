d09059a0b66b4d1df0a56966350934ae61df7cac
brian <brian@365mechanix.com>
2026-05-17 21:21:55 +1000

    Refactor Terraform workflow to improve artifact publishing and enhance string output formatting

--- providers.tf
+++ providers.tf
@@ -8,11 +8,11 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-developer-dev"
     storage_account_name = "stcollsysnpeaue00"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
+    use_azuread_auth     = true
   }
 }
 
