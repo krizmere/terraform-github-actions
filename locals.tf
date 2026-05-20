1f3d31a392de08e70724265b1fef2efa4a754d1c
brian <brian@365mechanix.com>
2026-05-20 22:14:22 +1000

    update: refactor resource group name handling and remove unused variables

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  project_name    = "terraform-github-actions"
-  location        = "australiaeast"
-  location_suffix = "aue"
+  project_name         = "terraform-github-actions"
+  location             = "australiaeast"
+  location_suffix      = "aue"
+  resource_group_name = "rg-${local.project_name}-${var.environment}-${local.location_suffix}"
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-${local.project_name}-${var.environment}-${local.location_suffix}"
+  name     = local.resource_group_name
   location = local.location
   tags     = var.tags
 }
